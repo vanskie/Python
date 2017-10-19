5fe1523961092b87b7508c7a70fe8756f1384d01
vanskie <vasilevs.ivan@gmail.com>
2017-10-19 03:03:39 +0300

    Some more excercises.

---
+++ ex2-/ex2-9.py
@@ -0,0 +1,4 @@
+favorite_number = 8
+print("The book wants me to reveal my favorite number to you now. Here we go. It's " + str(favorite_number))
+#The book wants me to write comments on two of my programs.
+#This was a comment
