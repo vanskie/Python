106cd45399e500969ac14a6af5e19136c6af5e97
Ivan <vasilevs.ivan@gmail.com>
2017-11-17 18:30:11 +0200

    Sync

---
+++ ex4-/ex4-13.py
@@ -0,0 +1,7 @@
+menu = ('soup', 'meatloaf', 'spaghetti', 'bread', 'pizza\n')
+for food in menu:
+	print(food)
+
+menu = ('Tuna', 'ramen', 'spaghetti', 'bread', 'pizza')
+for food in menu:
+	print(food)
